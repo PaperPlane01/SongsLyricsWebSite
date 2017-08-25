ec2a567449a7b38a7a015f0497f128183eced1ae
PaperPlane01 <dagfortnewage@gmail.com>
2017-08-26 03:27:50 +0600

    Minor fixes of Controller class
    Unused import statements removed;
    @WebServlet annotation removed;

--- src/main/java/kz/javalab/songslyricswebsite/controller/Controller.java
+++ src/main/java/kz/javalab/songslyricswebsite/controller/Controller.java
@@ -1,31 +1,18 @@
 package kz.javalab.songslyricswebsite.controller;
 
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
 import kz.javalab.songslyricswebsite.command.ActionCommand;
 import kz.javalab.songslyricswebsite.command.factory.ActionFactory;
 import kz.javalab.songslyricswebsite.conntectionpool.ConnectionPool;
-import kz.javalab.songslyricswebsite.model.song.Song;
-import kz.javalab.songslyricswebsite.model.song.lyrics.SongLyrics;
-import kz.javalab.songslyricswebsite.model.song.lyrics.SongLyricsComposite;
-import kz.javalab.songslyricswebsite.resource.ConfigurationManager;
-import kz.javalab.songslyricswebsite.service.SongsService;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 /**
  * Created by PaperPlane on 15.07.2017.
  */
-@WebServlet(name = "Controller")
 public class Controller extends HttpServlet {
 
     @Override
